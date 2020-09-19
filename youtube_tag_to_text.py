cf64a6e7ccf85888755cfc6d52609d07ce8ce909
liam-herbst <71550927+liam-herbst@users.noreply.github.com>
2020-09-19 07:08:32 -0400

    Youtube Tag Formatting Script

---
+++ youtube_tag_to_text.py
@@ -0,0 +1,8 @@
+#Insert tags inside ' ' below
+tags = 'notion,notion backlinks,notion back links,backlinks,back links,roam,roam vs notion,notion vs roam,backlinked notes,networked notes,zettelkasten,notion page,notion setup,notion organization,roam backlinks,best note-taking app,top note-taking app,notion page linking,notion linked pages,notion guide,notion tutorial,zettelkasten notion,notion zettelkasten,digital zettelkasten,notion back link,backlinks notion,notion new features,backlink notion'
+
+list = tags.split(",")
+
+text = ", ".join(list)
+
+print(text)
