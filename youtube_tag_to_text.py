d89c58f3a9cf97bc6f1128319629618bae724d9c
liam-herbst <71550927+liam-herbst@users.noreply.github.com>
2020-09-19 07:10:49 -0400

    Update youtube_tag_to_text.py

--- youtube_tag_to_text.py
+++ youtube_tag_to_text.py
@@ -1,5 +1,5 @@
 #Insert tags inside ' ' below
-tags = 'notion,notion backlinks,notion back links,backlinks,back links,roam,roam vs notion,notion vs roam,backlinked notes,networked notes,zettelkasten,notion page,notion setup,notion organization,roam backlinks,best note-taking app,top note-taking app,notion page linking,notion linked pages,notion guide,notion tutorial,zettelkasten notion,notion zettelkasten,digital zettelkasten,notion back link,backlinks notion,notion new features,backlink notion'
+tags = ''
 
 list = tags.split(",")
 
